a07a450bc9ad8fc7ebf11a9259c83cb01c401257
Oleg Gamega <gadfil12@gmail.com>
2018-03-13 14:03:49 +0300

    clear all

--- src/actions/auth.js
+++
@@ -1,7 +0,0 @@
-import {AsyncStorage} from "react-native";
-import {SET_AUTH_ID} from "../utils/constants";
-
-export const authInit = () => async (dispatch) => {
-    const id = JSON.parse(await AsyncStorage.getItem("@auth_id")) || null;
-    dispatch({type: SET_AUTH_ID, payload: id})
-}
