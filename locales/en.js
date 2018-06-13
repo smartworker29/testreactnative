b597d1e63caa486cfe2af1c8d3815c0ab2956da4
marqueewinq <marqueewinq@gmail.com>
2018-06-13 16:15:39 +0300

    added en translation

--- locales/en.js
+++ locales/en.js
@@ -1,134 +1,134 @@
 export default {
     login: {
-        login_button: 'Войти',
-        signup_button: 'Зарегестрироваться'
+        login_button: 'Login', // 'Войти',
+        signup_button: 'Sign up', // 'Зарегестрироваться'
     },
     user_profile: {
-        title: 'Профиль',
-        instanceName: "Раздел",
-        createAgent: "Создание агента",
-        updateAgent: "Обновление агента",
-        sync: "Синхронизация"
+        title: 'Profile', // 'Профиль',
+        instanceName: 'Section', // "Раздел",
+        createAgent: 'Create new agent', // "Создание агента",
+        updateAgent: 'Update agent', // "Обновление агента",
+        sync: 'Sync', // "Синхронизация"
     },
     visits_list: {
-        title: 'Отчёты',
-        emptyListMessageTitle: 'Пока нет отчетов',
-        emptyListMessageDetail: 'Чтобы добавить свой первый отчет откройте вкладку задания',
-        newVisit: 'Новый отчёт',
-        today: 'Сегодня',
-        before: 'Ранее',
-        lastSync: 'Последняя синхронизация данных',
-        moderation: 'Модерация',
-        OnModeration: "На модерации",
-        resultBad: 'Не засчитан',
-        resultGood: 'Засчитан',
-        route: "маршрут"
+        title: 'Reports', // 'Отчёты',
+        emptyListMessageTitle: 'No reports yet', // 'Пока нет отчетов',
+        emptyListMessageDetail: 'Open "Tasks" tab to begin', // 'Чтобы добавить свой первый отчет откройте вкладку задания',
+        newVisit: 'New report', // 'Новый отчёт',
+        today: 'Today', // 'Сегодня',
+        before: 'Before', // 'Ранее',
+        lastSync: 'Last sync at', // 'Последняя синхронизация данных',
+        moderation: 'Moderation', // 'Модерация',
+        OnModeration: 'On moderation', // "На модерации",
+        resultBad: 'Rejected', // 'Не засчитан',
+        resultGood: 'OK', // 'Засчитан',
+        route: 'Route', // "маршрут"
     },
     visitDetail: {
-        title: 'Отчёт',
-        agent: 'Агент',
-        shop: 'Магазин',
-        photo: 'Фотографии',
-        started_date: 'Дата отчёта',
-        offline: 'не синхронизированно',
-        message: 'Отчёт',
-        moderationResult: 'Результат модерации',
-        moderationDone: 'Модерация пройдена',
-        moderationReject: 'Модерация не пройдена',
-        moderationComment: 'Комментарий модератора:',
-        visitAccept: 'Засчитан',
-        visitReject: 'Не засчитан',
-        visitResult: 'Результаты отчёта'
+        title: 'Report', // 'Отчёт',
+        agent: 'Agent', // 'Агент',
+        shop: 'Store', // 'Магазин',
+        photo: 'Photos', // 'Фотографии',
+        started_date: 'Date', // 'Дата отчёта',
+        offline: 'Not synced yet', // 'не синхронизированно',
+        message: 'Report', // 'Отчёт',
+        moderationResult: 'Moderation status', // 'Результат модерации',
+        moderationDone: 'Moderation passed', // 'Модерация пройдена',
+        moderationReject: 'Moderation not passed', // 'Модерация не пройдена',
+        moderationComment: 'Moderator comment', // 'Комментарий модератора:',
+        visitAccept: 'OK', // 'Засчитан',
+        visitReject: 'Rejected', // 'Не засчитан',
+        visitResult: 'Visit results', // 'Результаты отчёта'
     },
     CreateVisit: {
-        title: 'Новый отчёт',
-        photo: 'Фотографии',
-        createAction: 'Начать',
-        backAction: 'назвд',
-        label: 'ID  Торговой точки',
-        description: 'Чтобы начать новый отчёт, укажите ниже ID магазина',
-        getGeo: "Получение геопозиции",
-        okGeo: "Геопозиция полученна",
-        createVisit: "Создание отчёта"
+        title: 'New report', // 'Новый отчёт',
+        photo: 'Photos', // 'Фотографии',
+        createAction: 'Start', // 'Начать',
+        backAction: 'Back', // 'назвд',
+        label: 'Store ID', // 'ID  Торговой точки',
+        description: 'Specify store ID to begin', // 'Чтобы начать новый отчёт, укажите ниже ID магазина',
+        getGeo: 'Updating geolocation', // "Получение геопозиции",
+        okGeo: 'Geolocation updated', // "Геопозиция полученна",
+        createVisit: 'Creating report', // "Создание отчёта"
     },
     settings: {
-        title: 'Настройки',
-        surname: 'Фамилия',
-        name: 'Имя',
-        patronymic: 'Отчество',
-        path_number: 'Номер маршрута',
-        contact_number: 'Контактный номер',
-        build: 'Версия сборки'
+        title: 'Settings', // 'Настройки',
+        surname: 'Surname', // 'Фамилия',
+        name: 'Name', // 'Имя',
+        patronymic: 'Last Name', // 'Отчество',
+        path_number: 'Route ID', // 'Номер маршрута',
+        contact_number: 'Contact Tel. Number', // 'Контактный номер',
+        build: 'Build version', // 'Версия сборки'
     },
     photo: {
-        title: 'Фото',
-        reship: 'Переснять',
-        makePhoto: 'Сделать фото',
-        noPhotoTitle: 'Пока нет фотографий',
-        expiredVisit: "Отчёт завершен",
-        noPhotoDescription: 'Чтобы сделать свой первый снимок нажмите кнопку ниже',
-        noPhotoExpiredVisit: "Чтобы сделать фотографии начните новый отчёт",
-        send: 'Отправить',
-        back: 'Завершить',
-        cancel: 'Отмена',
-        ok: 'Готово',
-        sync: 'Идет синхронизация',
-        allowAccess: "Разрешите доступ к камере"
+        title: 'Photo', // 'Фото',
+        reship: 'Remake', // 'Переснять',
+        makePhoto: 'Make Photo', // 'Сделать фото',
+        noPhotoTitle: 'No photos yet', // 'Пока нет фотографий',
+        expiredVisit: 'Visit completed', // "Отчёт завершен",
+        noPhotoDescription: 'Tap a button below to capture your first photo', // 'Чтобы сделать свой первый снимок нажмите кнопку ниже',
+        noPhotoExpiredVisit: 'Create another visit to capture another photo', // "Чтобы сделать фотографии начните новый отчёт",
+        send: 'Send', // 'Отправить',
+        back: 'Back', // 'Завершить',
+        cancel: 'Cancel', // 'Отмена',
+        ok: 'OK', // 'Готово',
+        sync: 'Sync in progress', // 'Идет синхронизация',
+        allowAccess: 'Please allow access to your camera', // "Разрешите доступ к камере"
     },
     error: {
-        attention: "Внимание",
-        emptyName: "Введите имя",
-        emptySurname: "Введите фамилию",
-        emptyPathNumber: "Введите Номер маршрута",
-        emptyPatronymic: "Введите отчество",
-        agentCreation: "Неудалось создать агента, попробуйте ещё раз",
-        geoDeny: "В телефоне отключенна геолокация",
-        createAgent: "Ошибка создания агента",
-        updateAgent: "Ошибка обновления агента"
+        attention: 'Attention', // "Внимание",
+        emptyName: 'Enter your name', // "Введите имя",
+        emptySurname: 'Enter your surname', // "Введите фамилию",
+        emptyPathNumber: 'Enter your route ID', // "Введите Номер маршрута",
+        emptyPatronymic: 'Enter your last name', // "Введите отчество",
+        agentCreation: 'Couldn\'t create an agent, try again', // "Неудалось создать агента, попробуйте ещё раз",
+        geoDeny: 'Geolocation is disabled in your phone', // "В телефоне отключенна геолокация",
+        createAgent: 'Error while creating agent', // "Ошибка создания агента",
+        updateAgent: 'Error while updating agent', // "Ошибка обновления агента"
     },
     pin: {
-        enter: "Введите пин-код",
-        enterAgain: "Укажите пин-код повторно",
-        error: "Неверный пин-код",
-        description: "Если у вас возникли сложности, то свяжитесь с нами по номеру\n 8 (800) 550-27-78 (звонок беплатный)"
+        enter: 'Enter PIN', // "Введите пин-код",
+        enterAgain: 'Enter PIN again', // "Укажите пин-код повторно",
+        error: 'Incorrect PIN', // "Неверный пин-код",
+        description: 'Having trouble with login?\nCall 8 (800) 550-27-78 (free)', // "Если у вас возникли сложности, то свяжитесь с нами по номеру\n 8 (800) 550-27-78 (звонок беплатный)"
     },
     task: {
-        title: "Задачи",
-        instruction: "Подробная инструкция",
+        title: 'Tasks', // "Задачи",
+        instruction: 'Instruction', // "Подробная инструкция",
 
     },
     tasks: {
-        emptyTitle: "Пока нет заданий",
-        emptyDescription: "Мы уведомим вас при поступлении новых заданий"
+        emptyTitle: 'No tasks yet', // "Пока нет заданий",
+        emptyDescription: 'We will notify you upon receiving new tasks', // "Мы уведомим вас при поступлении новых заданий"
     },
     tabBar: {
-        tasks: "Задания",
-        results: "Результат ",
-        reports: "Отчёты",
-        profile: "Профиль"
+        tasks: '', // "Задания",
+        results: '', // "Результат ",
+        reports: '', // "Отчёты",
+        profile: '', // "Профиль"
     },
     sync: {
-        title: "Состояние"
+        title: '', // "Состояние"
     },
     reports: {
-        underNum: "Успешно засчитанных отчетов",
-        unassigned: "Незасчитанных отчетов",
-        moderationPass: "Модерация пройдена",
-        moderationUnPass: "Модерация не пройдена",
-        onModeration: "На модерации"
+        underNum: '', // "Успешно засчитанных отчетов",
+        unassigned: '', // "Незасчитанных отчетов",
+        moderationPass: '', // "Модерация пройдена",
+        moderationUnPass: '', // "Модерация не пройдена",
+        onModeration: '', // "На модерации"
     },
     preview: {
-        title: "Предпросмотр",
-        remove: "Удаление фото"
+        title: '', // "Предпросмотр",
+        remove: '', // "Удаление фото"
     },
     feedback: {
-        support: "Служба поддержки",
-        title: "Жалоба",
-        send: "Отправить",
-        answer: "Ответ на жалобу",
-        request: "Отправка жалобы",
-        success: "Жалоба успешно отправлена",
-        error: "Жалоба не отправлена",
-        describe: "Опишите ситуацию"
+        support: '', // "Служба поддержки",
+        title: '', // "Жалоба",
+        send: '', // "Отправить",
+        answer: '', // "Ответ на жалобу",
+        request: '', // "Отправка жалобы",
+        success: '', // "Жалоба успешно отправлена",
+        error: '', // "Жалоба не отправлена",
+        describe: '', // "Опишите ситуацию"
     }
 }
