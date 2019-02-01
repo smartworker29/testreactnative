a6e93a9f3ca4e4cf3d7be10c3062237409487cb1
Ivan Fokeev <vanya@vanya.cc>
2019-02-01 14:54:20 +0300

    Oleg deleted this file))

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
-import bugsnag from './src/bugsnag'
 
 // let e = undefined.length
 AppRegistry.registerComponent('Papsi', () => App)
