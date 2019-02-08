a08bd1472efdc38f2e9ca0f115c1ebee3bbaba1b
Ivan Fokeev <vanya@vanya.cc>
2019-02-08 18:07:38 +0300

    fix sentry DNS url & add buildContext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, {Component} from 'react'
 import {Provider} from 'react-redux'
 import {Root} from 'native-base'
-import {configureStore} from './configStore'
-import AppWithNavigationState from './navigators/AppNavigator'
 import I18n from 'react-native-i18n'
-import en from '../locales/en'
-import ru from '../locales/ru'
+import DeviceInfo from 'react-native-device-info'
 import Orientation from "react-native-orientation";
 import {Sentry} from 'react-native-sentry'
+
+import en from '../locales/en'
+import ru from '../locales/ru'
+
+import {configureStore} from './configStore'
+import AppWithNavigationState from './navigators/AppNavigator'
+
 import ErrorBoundary from './component/ErrorBoundary'
 
-if (!__DEV__) {
-  Sentry.config('https://11d80528d8d744ce9098692402b0339f@sentry.inspector-cloud.ru//6').install();
+const dsn = 'https://11d80528d8d744ce9098692402b0339f@sentry.inspector-cloud.ru/6'
+
+const setupSentry = () => {
+  Sentry.config(dsn).install()
+  addBuildContext()
 }
+
+const addBuildContext = () => {
+  Sentry.setTagsContext({
+    appVersion: DeviceInfo.getVersion(),
+    buildNumber: DeviceInfo.getBuildNumber(),
+    deviceInfo: {
+      systemName: DeviceInfo.getSystemName(),
+      systemVersion: DeviceInfo.getSystemVersion(),
+      deviceName: DeviceInfo.getDeviceName()
+    }
+  })
+}
+
+if (!__DEV__) {
+  setupSentry();
+}
+
 const currentLocale = I18n.currentLocale();
 
 const store = configureStore();
